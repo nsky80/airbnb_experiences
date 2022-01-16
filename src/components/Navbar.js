03a4e62ee5249c6f5798f9ab11483f6092d2ff65
Satish NSKY80 <satishkumary80@gmail.com>
2022-01-17 00:32:36 +0530

    Initializing the project with Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+export default function Navbar(){
+    return (
+        <nav>
+            Hello World!
+        </nav>
+    )
+}
