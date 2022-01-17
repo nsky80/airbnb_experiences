1502a0bf92d1107606b9e08ceda5a01c114253dc
Satish NSKY80 <satishkumary80@gmail.com>
2022-01-17 13:43:01 +0530

    Hero element added with responsive layout

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import photo_grid from "../images/photo-grid.png"
+
+export default function Hero(){
+    return (
+        <section className="hero">
+            <img src={photo_grid} alt="grid shots" className="hero--photo-grid"/>
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">
+                Join unique interactive activites led by
+                one-of-a-kind hosts-all without leaving 
+                home.
+            </p>
+        </section>
+    )
+}
