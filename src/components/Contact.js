6ed2b260a865f160762e209422ce098bdcbd41fd
Satish NSKY80 <satishkumary80@gmail.com>
2022-01-18 00:36:59 +0530

    destructuring the props

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,19 +2,23 @@
 import phone_icon from "../images/phone-icon.png" 
 import mail_icon from "../images/mail-icon.png"
 
-export default function Contact(props) {
+/*
+We can de-strucure the props like
+*/
+
+export default function Contact({img, name, phone, email}) {
     // console.log(props)
     return (
         <div className="contact-card">
-            <img src={props.img}/>
-            <h3>{props.name}</h3>
+            <img src={img}/>
+            <h3>{name}</h3>
             <div className="info-group">
                 <img src={phone_icon} />
-                <p>{props.phone}</p>
+                <p>{phone}</p>
             </div>
             <div className="info-group">
                 <img src={mail_icon} />
-                <p>{props.email}</p>
+                <p>{email}</p>
             </div>
         </div>
     )
