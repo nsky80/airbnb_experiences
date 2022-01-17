339cbe01ab0b32ace34c92e1023b51e365dd4613
Satish NSKY80 <satishkumary80@gmail.com>
2022-01-18 00:14:55 +0530

    formal param of props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import Navbar from './components/Navbar';
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 import Contact from './components/Contact';
+import mr_whiskerson from "./images/mr-whiskerson.png"
+import fluffykins from "./images/fluffykins.png"
+import felix from "./images/felix.png"
+import Pumpkin from "./images/pumpkin.png"
+/*
+This is how props will received at Contact function:
+
+{
+    img: "./images/mr-whiskerson.png", 
+    name: "Mr. Whiskerson", 
+    phone: "(212) 555-1234", 
+    email: "mr.whiskaz@catnap.meow"
+}
+
+*/
+
 
 function App(){
   return (
@@ -12,25 +28,25 @@ function App(){
       {/* <Card /> */}
 
       <Contact 
-                img="./images/mr-whiskerson.png"
+                img={mr_whiskerson}
                 name="Mr. Whiskerson"
                 phone="(212) 555-1234"
                 email="mr.whiskaz@catnap.meow"
             />
             <Contact 
-                img="./images/fluffykins.png"
+                img={fluffykins}
                 name="Fluffykins"
                 phone="(212) 555-2345"
                 email="fluff@me.com"
             />
             <Contact 
-                img="./images/felix.png"
+                img={felix}
                 name="Felix"
                 phone="(212) 555-4567"
                 email="thecat@hotmail.com"
             />
             <Contact 
-                img="./images/pumpkin.png"
+                img={Pumpkin}
                 name="Pumpkin"
                 phone="(0800) CAT KING"
                 email="pumpkin@scrimba.com"
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,20 @@
-import mr_whiskerson from "../images/mr-whiskerson.png"
+
 import phone_icon from "../images/phone-icon.png" 
 import mail_icon from "../images/mail-icon.png"
 
-export default function Contact() {
+export default function Contact(props) {
+    // console.log(props)
     return (
         <div className="contact-card">
-            <img src={mr_whiskerson}/>
-            <h3>Mr. Whiskerson</h3>
+            <img src={props.img}/>
+            <h3>{props.name}</h3>
             <div className="info-group">
                 <img src={phone_icon} />
-                <p>(212) 555-1234</p>
+                <p>{props.phone}</p>
             </div>
             <div className="info-group">
                 <img src={mail_icon} />
-                <p>mr.whiskaz@catnap.meow</p>
+                <p>{props.email}</p>
             </div>
         </div>
     )
